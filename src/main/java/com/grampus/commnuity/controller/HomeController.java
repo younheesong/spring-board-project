24942f7a490d7c1d064c3551e80e3dcd2544286d
rosie <rosie@grampus.co>
2023-09-13 11:35:58 +0900

    fix: / redirect list page

--- src/main/java/com/grampus/commnuity/controller/HomeController.java
+++ src/main/java/com/grampus/commnuity/controller/HomeController.java
@@ -4,13 +4,17 @@ package com.grampus.commnuity.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
     @GetMapping("/")
     public String homePage(){
-        return "/home";
+
+        return "redirect:/boards/java";
     }
+
 
 }
