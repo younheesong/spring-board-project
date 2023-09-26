6d9bf01af31734fb5e713eededf059a25a3f041c
rosie <rosie@grampus.co>
2023-09-26 12:04:56 +0900

    fix: change file service logic

--- src/main/java/com/grampus/commnuity/service/FileService.java
+++ src/main/java/com/grampus/commnuity/service/FileService.java
@@ -1,10 +1,11 @@
 package com.grampus.commnuity.service;
 
 
-import com.grampus.commnuity.domain.Board;
 import com.grampus.commnuity.domain.File;
 import com.grampus.commnuity.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,79 +15,105 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FileService {
     @Value("${file.dir}")
     private String fileDir;
 
     private final FileRepository fileRepository;
 
-    public String getFullPath(String filename){
-        return fileDir +filename;
+    public String getFullPath(String filename) {
+        return fileDir + filename;
     }
+
+    // 파일 가져오기
+    public List<File> getFiles(long boardId) {
+        List<File> files = fileRepository.getFilesByBoardId(boardId);
+        return files;
+    }
+
     // 다중 파일 저장
-    public List<File> saveFiles(List<MultipartFile> files, Board board){
-        List<File> storeFileResults = new ArrayList<>();
-        for (MultipartFile file: files){
-            if(!file.isEmpty()){
-                storeFileResults.add(saveFile(file, board));
+    public void saveFiles(List<MultipartFile> files, Long boardId) {
+        for (MultipartFile file : files) {
+            if (!file.isEmpty()) {
+                saveFile(file, boardId);
             }
         }
-        return storeFileResults;
     }
+
     // 단일 파일 저장
     @Transactional
-    public File saveFile(MultipartFile file, Board board){
-        if(file.isEmpty()){
-            return null;
+    public void saveFile(MultipartFile file, Long boardId) {
+        if (file.isEmpty()) {
+            return;
         }
 
         String originalFilename = file.getOriginalFilename();
         String savedFilename = createStoreFilename(originalFilename);
 
-        try{
+        createFilePath(file, savedFilename);
+
+        fileRepository.saveFile(File.builder()
+                .boardId(boardId)
+                .savedFileName(savedFilename)
+                .originalFileName(originalFilename)
+                .build());
+
+    }
+
+    public void createFilePath(MultipartFile file, String savedFilename){
+        try {
             Path path = Paths.get(fileDir, savedFilename);
             Files.createFile(path);
             file.transferTo(path); // 파일 저장
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return fileRepository.save(File.builder()
-                        .board(board)
-                .originalFileName(originalFilename)
-                .savedFileName(savedFilename)
-                .build());
     }
 
     // 서버에 저장할 이름 생성
-    private String createStoreFilename(String originalFilename){
+    public String createStoreFilename(String originalFilename) {
         String ext = extractExt(originalFilename);
         return UUID.randomUUID() + "." + ext;
     }
 
     // 확장자 추출
-    private String extractExt(String originalFilename) {
+    public String extractExt(String originalFilename) {
         int pos = originalFilename.lastIndexOf(".");
         return originalFilename.substring(pos + 1);
     }
 
 
-
     // 이미지 삭제
     @Transactional
     public void deleteFiles(List<File> files) throws IOException {
-        for (File file: files){
+        for (File file : files) {
             deleteFile(file);
         }
     }
+
     @Transactional
-    public void deleteFile(File file) throws IOException {
-        fileRepository.delete(file);
-        Files.deleteIfExists(Paths.get(getFullPath(file.getSavedFileName())));
+    public void deleteFile(@NotNull File file) {
+        log.info("DELETE_FILE_ID : "+file.getId());
+        fileRepository.deleteFile(file.getId());
+        try {
+            Files.deleteIfExists(Paths.get(getFullPath(file.getSavedFileName())));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteFile(@NotNull String savedFilename) {
+        log.info("DELETE_SAVED_FILE_NAME : "+getFullPath(savedFilename));
+        try {
+            Files.deleteIfExists(Paths.get(getFullPath(savedFilename)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
