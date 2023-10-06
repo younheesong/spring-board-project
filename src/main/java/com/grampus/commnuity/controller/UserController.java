32caa7327c7a9602b69691f59bde5c7324d2699f
rosie <rosie@grampus.co>
2023-10-06 13:36:56 +0900

    feat: add 현재 유저 조회 api

--- src/main/java/com/grampus/commnuity/controller/UserController.java
+++ src/main/java/com/grampus/commnuity/controller/UserController.java
@@ -28,20 +28,20 @@ public class UserController {
 
 
     @GetMapping("/join")
-    public String joinPage(Model model){
+    public String joinPage(Model model) {
         model.addAttribute("userJoinDto", new UserJoinDto());
         return "/users/join";
     }
 
     @PostMapping("/join")
     @ResponseBody
-    public ResponseEntity<String> join(@RequestBody UserJoinDto userJoinDto ){
+    public ResponseEntity<String> join(@RequestBody UserJoinDto userJoinDto) {
         log.info(userJoinDto.toString());
 
         // 중복 검사
-        if(userService.existsDuplicatedUser(userJoinDto)){
+        if (userService.existsDuplicatedUser(userJoinDto)) {
             log.info("중복 존재");
-           return new ResponseEntity<>("fail", HttpStatus.CONFLICT);
+            return new ResponseEntity<>("fail", HttpStatus.CONFLICT);
         }
 
         userService.join(userJoinDto);
@@ -49,10 +49,10 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String loginPage(HttpServletRequest request){
+    public String loginPage(HttpServletRequest request) {
         // 로그인 성공 시, 이전 페이지로 이동하기 위해 이전 페이지 저장
         String uri = request.getHeader("Referer");
-        if(uri!=null && !uri.contains("/login") && !uri.contains("/join")){
+        if (uri != null && !uri.contains("/login") && !uri.contains("/join")) {
             request.getSession().setAttribute("prevPage", uri);
         }
         return "/users/login";
@@ -61,7 +61,7 @@ public class UserController {
     @GetMapping("/boards")
     public String userBoardsPage(Model model,
                                  @RequestParam(required = false, defaultValue = "1") int page,
-                                 Authentication auth){
+                                 Authentication auth) {
 
         UserDetail userDetail = (UserDetail) auth.getPrincipal();
         Long userId = userDetail.getId();
@@ -76,5 +76,16 @@ public class UserController {
         model.addAttribute("totalPage", pagination.getTotalPage());
         return "/users/boards";
     }
+
+
+    @GetMapping("/me")
+    @ResponseBody
+    public ResponseEntity<Long> getCurrentUserId(Authentication auth) {
+        if (auth != null && auth.isAuthenticated()) {
+            return new ResponseEntity<>(((UserDetail) auth.getPrincipal()).getId(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+    }
+
 
 }
